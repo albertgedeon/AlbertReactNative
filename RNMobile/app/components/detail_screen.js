9f56ae00adfe6c7678d81606e9c8f0e66e33e543
Albert Gedeon <albertgedeon@Alberts-Mac-mini.local>
2019-11-08 15:27:46 -0700

    Got address and company name displayed on detail screen

--- RNMobile/app/components/detail_screen.js
+++ RNMobile/app/components/detail_screen.js
@@ -8,14 +8,21 @@ import { withNavigation } from 'react-navigation';
 class DetailScreen extends React.Component {
     constructor(props) {
         super(props);
-        console.log(props.navigation.getParam('company'));
-        this.state = {};
+        this.state = {
+          company: props.navigation.getParam('company')
+        };
     }
 
     render() {
+      console.log(this.state.company)
         return (
           <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Details Screen</Text>
+            <Text>Company Name: {this.state.company.company_name}</Text>
+            <Text>Street: {this.state.company.address.street}</Text>
+            <Text>Postal Code: {this.state.company.address.postal_code}</Text>
+            <Text>City: {this.state.company.address.city}</Text>
+            <Text>State: {this.state.company.address.state}</Text>
+            <Text>Country: {this.state.company.address.country}</Text>
           </View>
         );
       }
